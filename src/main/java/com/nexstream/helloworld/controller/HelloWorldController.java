915066f4c482cd1452f283e4d20212457c027d75
chansm <siewmeng.chan@appxtream.com>
2015-11-11 11:55:45 +0800

    first commit

---
+++ src/main/java/com/nexstream/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,48 @@
+package com.nexstream.helloworld.controller;
+ 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloWorldController {
+	String message = "Welcome to Spring MVC!";
+	String message3 = "try 3";
+	
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			//@RequestParam(value = "name", required = false, defaultValue = "World") String name
+			//@RequestParam(value = "message2", required = false, defaultValue = "message2") String message2
+			@RequestParam Map<String, String> requestparam
+			) {
+		System.out.println("in controller");
+		
+		String name = requestparam.get("name");
+		String message2 = requestparam.get("message2");
+		//String message4 = requestparam.get("message4");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		mv.addObject("message2", message2);
+		mv.addObject("message3", message3);
+		//mv.addObject("message4", message4);
+		return mv;
+	}
+	
+	@RequestMapping("/bello")
+	public ModelAndView showMessage2(
+			@RequestParam Map<String, String> requestparam2){
+		String message4 = requestparam2.get("message4");
+		
+		ModelAndView mv2 = new ModelAndView("helloworld");
+		mv2.addObject("message4", message4);
+		return mv2;
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@page language = "java" contentType = "text/html; charset=ISO-8859-1" pageEncoding = "ISO-8859-1" %>
+
+<html>
+<head>
+	<meta http-equiv = "Content-Type" content = "text/html; charset=ISO-8859-1">
+	<title>Spring mvc - hello world index page</title>
+</head>
+<body>
+<h2>Hello World! send</h2>
+	<h3>
+		<a href = "hello?name=Eric&message2=try2&message4=try4">link here</a>
+		<br>
+		<a href = "bello?message4=try4">link2 here</a>
+	</h3>
+</body>
+</html>
